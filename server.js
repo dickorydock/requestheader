9fbd2527e82781c1483841064bc2984664eb76aa
Dickory Dock <nickcommins@gmail.com>
2016-08-03 09:10:57 -0400

    changing outputs 1

--- server.js
+++ server.js
@@ -39,7 +39,8 @@ app.get('/:name', function(req,res){
 	}
     console.log(req);
     console.log("donezo");
-	res.send(Object.getOwnPropertyNames(req));
+	// res.send(Object.getOwnPropertyNames(req));
+    res.send(JSON.stringify(req))
 })
 
 app.listen(process.env.PORT, function(){
